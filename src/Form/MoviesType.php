62270356c0301985ff0f92cede52b0c9c8738fd0
Said785 <98874257+Said785@users.noreply.github.com>
2022-02-03 17:32:32 +0100

    ajouter et modifier

--- src/Form/MoviesType.php
+++ src/Form/MoviesType.php
@@ -5,18 +5,47 @@ namespace App\Form;
 use App\Entity\Movies;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MoviesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('description')
-            ->add('dateAt')
-            ->add('image')
-        ;
+        ->add('titre', TextType::class, [ "label" => "Titre du films",
+        "required" => false,
+        "attr" => [
+            "placeholder" => " ajouter le titre du film"
+            
+        ],
+        "constraints" => [
+            new NotBlank([
+                "message" => "Veuillez entrer un titre"
+            ]),
+            new Length([
+                "min" => 3,
+                "max" => 20,
+                "minMessage" => "Veuillez saisir 3 caractères minimum",
+                "maxMessage" => "Veuillez saisir 20 caractères maximum"
+            ])
+        ]
+        ])
+
+        ->add('description', TextareaType::class, [
+            "label" => "Description ",
+            "required" => false,
+            "attr" => [
+                "row" => 4
+            ]
+            ]);
+        
+        
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
