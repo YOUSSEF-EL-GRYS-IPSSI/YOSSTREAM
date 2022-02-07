8991c884c8c431aa3fee790197d4aa55ab07b9b1
Said785 <98874257+Said785@users.noreply.github.com>
2022-02-07 15:36:24 +0100

    form inscription
    j'ai mis des contrainte dans le formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,13 +3,14 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
@@ -18,14 +19,30 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('email')
+        $builder 
+
+            ->add('email',EmailType::class,[
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'veuillez saisir une adresse mail'],
+                'constraints' => [new Length(['max' => 60])],
+            ])
             ->add('name',TextType::class,[
-                'label'=> 'Votre Nom'
+                'label'=> 'Votre Nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ],
+                'constraints' => [new Length(['min' => 3 , 'max' => 50])],
             ])
+          
             ->add('username',TextType::class,[
-                'label'=> 'Votre Prénom'
+                'label'=> 'Votre Prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre Prenom'
+                ],
+                'constraints' => [new Length(['min' => 3 , 'max' => 50])],
             ])
+           
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -34,19 +51,23 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'label'=>"Votre Mot de passe",
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                            'placeholder' => 'veuillez saisir un mot de pass'],
+                
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Entrez un mot de passe',
+                        
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit avoir {{ 6 }} caractères',
+                        'minMessage' => 'Votre mot de passe doit avoir  6  caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
